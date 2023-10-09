93fefaee0845f183be4545bfd2a95ef272f294df
Alex Dulac <alex-dulac@idexx.com>
2023-10-09 09:15:16 -0400

    rsvp-server

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # rsvp-server
+
+Small backend app to use with: https://github.com/alex-dulac/rsvp-app
+
+It authenticates an oauth client to be used with a Google Cloud API Service: https://console.cloud.google.com/apis
+
+Features:
+- Node
+- Express
+- Googleapis
+- Oauth2
+
+To start the server, run the following in the project directory:
+
+### `node app.js`
+
--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 /**
- * Express app setup
+ * This script is intended to be a server to be used with front-end app: https://github.com/alex-dulac/rsvp-app
+ *
+ * It authenticates an oauth client to be used with google drive.
+ * The idea is that after the event that folks RSVP'd to,
+ * they can visit the site again and see all public pictures uploaded to drive.
+ *
+ * General app setup
  */
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+
+require('dotenv').config();
+
+const axios = require('axios');
+const request = require('request');
+const cors = require('cors');
+const urlParse = require('url-parse');
+import * as queryParse from 'querystring';
+const bodyParser = require('body-parser');
+
+/**
+ * Express app
+ */
 const express = require('express');
 const app = express();
 const port = 8080;
+app.listen(port, () => {});
 
 /**
  * Google API integration
@@ -13,26 +33,21 @@ const port = 8080;
 const { google } = require('googleapis');
 const fs = require('fs').promises;
 const process = require('process');
+const opn = require('opn');
 const os = require('os');
 const uuid = require('uuid');
 const path = require('path');
 const { authenticate } = require('@google-cloud/local-auth');
 
-// The file token.json stores the app's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-
 /**
- * Axios + env + other stuff
+ * Globals
  */
-require('dotenv').config();
-const axios = require('axios');
-const request = require('request');
-const cors = require('cors');
-const urlParse = require('url-parse');
-import * as queryParse from 'querystring';
-const bodyParser = require('body-parser');
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const CLIENT_ID = process.env.GOOGLE_API_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_API_CLIENT_SECRET;
+const REDIRECT_URI = process.env.GOOGLE_API_REDIRECT_URI;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -61,6 +76,56 @@ app.get('/photos', function (req, res) {
  * Authorize google api for use
  */
 app.get('/authorize', function (req, res) {
+    if (bypassAuth()) {
+        return;
+    }
+
+    const oauth2Client = getOAuth2Client();
+    const url = oauth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+        state: JSON.stringify({
+            callbackUrl: process.env.GOOGLE_API_REDIRECT_URL,
+        })
+    });
+
+    request(url, (error, response, body) => {
+        res.sendStatus(200);
+    });
+});
+
+/**
+ * Get a refresh token on redirect
+ */
+app.get('/tokens', async function (req, res) {
+    const url = new URL(process.env.APP_BASE_URL + req.url);
+    const code = url.searchParams.get('code'); // 'code' is the refresh token
+
+    const oauth2Client = getOAuth2Client();
+    const tokens = await oauth2Client.getToken(code);
+    console.log(tokens);
+});
+
+function getOAuth2Client() {
+    return new google.auth.OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        REDIRECT_URI
+    );
+}
+
+/**
+ * TODO
+ * We can skip authing with google if a valid refresh token is available
+ */
+function bypassAuth() {
+    return false;
+}
+
+/**
+ * Testing...
+ */
+app.get('/authorize-3000', function (req, res) {
     const oauth2Client = getOAuth2Client();
 
     const url = oauth2Client.generateAuthUrl({
@@ -77,9 +142,9 @@ app.get('/authorize', function (req, res) {
 });
 
 /**
- * Tokens
+ * Testing...
  */
-app.get('/tokens', async function (req, res) {
+app.get('/tokens-3000', async function (req, res) {
     const queryUrl = new urlParse(req.url);
     console.log(queryUrl);
     const code = queryParse.parse(queryUrl.query).code;
@@ -91,14 +156,65 @@ app.get('/tokens', async function (req, res) {
 });
 
 /**
- * Hey! Listen!
+ * test endpoint using google's quickstart guide: https://github.com/googleworkspace/node-samples/blob/main/drive/quickstart/index.js
+ * https://developers.google.com/drive/api/quickstart/nodejs
  */
-app.listen(port, () => {});
+app.get('/authorize-testing', function (req, res) {
+    authorize().then(listFiles).catch(console.error);
+});
 
-function getOAuth2Client() {
-    return new google.auth.OAuth2(
-        process.env.GOOGLE_API_CLIENT_ID,
-        process.env.GOOGLE_API_CLIENT_SECRET,
-        process.env.GOOGLE_API_REDIRECT_URL
-    );
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content.toString());
+        return google.auth.fromJSON(credentials);
+    } catch (err) {
+        return null;
+    }
+}
+
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content.toString());
+    const key = keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
+    return client;
+}
+
+async function listFiles(authClient) {
+    const drive = google.drive({version: 'v3', auth: authClient});
+    const res = await drive.files.list({
+        pageSize: 10,
+        fields: 'nextPageToken, files(id, name)',
+    });
+    const files = res.data.files;
+    if (files.length === 0) {
+        console.log('No files found.');
+        return;
+    }
+
+    console.log('Files:');
+    files.map((file) => {
+        console.log(`${file.name} (${file.id})`);
+    });
 }
